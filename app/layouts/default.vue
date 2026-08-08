a7eae43110204e3a4614060f13d5b9823bcb18c7
Seyed Erfan Hosseini Fadakari <hoseni.erfan85@gmail.com>
2026-08-08 16:15:03 +0330

    feat: add services page shell

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import Header from '~/components/Header.vue'
+
+const route = useRoute()
+const showServicesHeader = computed(() => route.path === '/services' || route.path.startsWith('/services/'))
+</script>
+
+<template>
+  <div class="site-layout">
+    <Header v-if="showServicesHeader" />
+    <slot />
+  </div>
+</template>
